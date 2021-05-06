2a779b5893f3da98467c358402b1e4a73ed0f3e1
Esraa Ahmed <48497082+esraaahmed6@users.noreply.github.com>
2021-05-06 19:13:46 +0200

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -12,9 +12,7 @@ import tweepy
 def clean(lines):
     words = lines.select(explode(split(lines.value, "t_end")).alias("word"))
     words = words.na.replace('', None)
-    words = words.na.drop()
-    words.printSchema()
-    
+    words = words.na.drop()    
     words= words.withColumn('user_id',f.split('word',';').getItem(0)).withColumn('timestamp', f.split('word', ';').getItem(1)).withColumn('followers_count', f.split('word', ';').getItem(2)).withColumn('location', f.split('word', ';').getItem(3)).withColumn('text', f.split('word', ';').getItem(4)).withColumn('retweet_count', f.split('word', ';').getItem(5)).withColumn('tweet_id', f.split('word', ';').getItem(6)).withColumn('user_name', f.split('word', ';').getItem(7))
     
     words.printSchema()
@@ -40,9 +38,9 @@ def text_classification(words):
 def reply_to_tweet(tweet):
     user = tweet.user_name
     if tweet.sentiment=="positive":
-      msg = "@%s Thanks for positivity" %user
+      msg = "@%s Thank you for spreading the positivity!" %user
     else:
-      msg = "@%s sorry for that you're feeling that way!" %user
+      msg = "@%s sorry for this feeling !" %user
     msg_sent = api.update_status(msg, tweet.tweet_id)
     print("--Reply Posted--")
     
@@ -50,23 +48,23 @@ def reply_to_tweet(tweet):
 if __name__ == "__main__":
 
     #reply on tweets
-    consumer_key = "xyRHPbiwuW9sWt1BSB86abNTp"
-    consumer_secret_key = "6oynAT9ZdF3U5ZzizYaSxYF7EQ4Ntk9TSoA6AgTrTT9IDCYvaI"
+    consumer_key = "wHGi2BtgLE5cGSn3AmAWl4QPd"
+    consumer_secret_key = "gPIyjlqhRX7Fh70l973n1qZxG7WJBWnx98sW2RKYgWrI04ogFp"
 
-    access_token = "1385679347428872192-TPO51sx9ZtmkjdgqhuKAgEvmWTuSUz"
-    access_token_secret = "Dv27qMuj9BO2nZyvWxMiPLKMBmxOGeNOGDceG80J7anm6"
-
+    access_token = "1385679347428872192-DVdTE8zU7IvAhD57sD3eGkh8Wl2vtG"
+    access_token_secret = "lnOnD4q6545sSb01QcMDUxaYQJtRfF5Hh5rILcgOmKIGS"
+ 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
     
     # create Spark session
-    spark = SparkSession.builder.appName("TwitterSentimentAnalysis").getOrCreate()
-    #spark.conf.set("spark.sql.parquet.compression.codec", "gzip")
+    spark = SparkSession.builder.appName("Twitter").getOrCreate()
+    spark.conf.set("spark.sql.parquet.compression.codec", "gzip")
 
     
-    # Subscribe to topic test1 
+    # Subscribe to test1 topic 
     lines = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "172.18.0.2:6667").option("subscribe", "test1").load()
     lines.printSchema()
     
@@ -83,7 +81,7 @@ if __name__ == "__main__":
     query = words.writeStream.queryName("all_tweets")\
         .outputMode("append").format("parquet")\
         .option("path", "hdfs://sandbox-hdp.hortonworks.com:8020/user/root/twitter")\
-        .option("checkpointLocation","hdfs://sandbox-hdp.hortonworks.com:8020/user/root/twitter")\
+        .option("checkpointLocation","hdfs://sandbox-hdp.hortonworks.com:8020/user/root/check")\
         .trigger(processingTime='60 seconds').start()
     query.awaitTermination()
     reply_query.awaitTermination()
