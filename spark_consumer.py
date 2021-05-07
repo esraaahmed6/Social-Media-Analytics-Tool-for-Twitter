9554b3913d854b466e5b9223f43c09347b196d8c
Esraa Ahmed <48497082+esraaahmed6@users.noreply.github.com>
2021-05-07 07:13:38 +0200

    Consumer

--- spark_consumer.py
+++ spark_consumer.py
@@ -8,13 +8,12 @@ import time
 import tweepy 
 
 
-    
+#clean data
 def clean(lines):
     words = lines.select(explode(split(lines.value, "t_end")).alias("word"))
     words = words.na.replace('', None)
     words = words.na.drop()    
-    words= words.withColumn('user_id',f.split('word',';').getItem(0)).withColumn('timestamp', f.split('word', ';').getItem(1)).withColumn('followers_count', f.split('word', ';').getItem(2)).withColumn('location', f.split('word', ';').getItem(3)).withColumn('text', f.split('word', ';').getItem(4)).withColumn('retweet_count', f.split('word', ';').getItem(5)).withColumn('tweet_id', f.split('word', ';').getItem(6)).withColumn('user_name', f.split('word', ';').getItem(7))
-    
+    words= words.withColumn('user_id',f.split('word',';').getItem(0)).withColumn('timestamp_time', f.split('word', ';').getItem(1)).withColumn('followers_count', f.split('word', ';').getItem(2)).withColumn('location', f.split('word', ';').getItem(3)).withColumn('tweet', f.split('word', ';').getItem(4)).withColumn('retweet_count', f.split('word', ';').getItem(5)).withColumn('tweet_id', f.split('word', ';').getItem(6)).withColumn('user_name', f.split('word', ';').getItem(7)) 
     words.printSchema()
     return words
 
@@ -48,11 +47,10 @@ def reply_to_tweet(tweet):
 if __name__ == "__main__":
 
     #reply on tweets
-    consumer_key = "wHGi2BtgLE5cGSn3AmAWl4QPd"
-    consumer_secret_key = "gPIyjlqhRX7Fh70l973n1qZxG7WJBWnx98sW2RKYgWrI04ogFp"
-
-    access_token = "1385679347428872192-DVdTE8zU7IvAhD57sD3eGkh8Wl2vtG"
-    access_token_secret = "lnOnD4q6545sSb01QcMDUxaYQJtRfF5Hh5rILcgOmKIGS"
+    consumer_key = "HgMvQsFRLSCnNsWZKzAWCC8sa"
+    consumer_secret_key = "aNc1QlRFWN0jpG5DWCjh6ib5KRPBMOI3uQYEKMH4kPH4JdzilS"
+    access_token = "1385696117938077700-2jxnaoZYujojv9kS7Se1Pegkjfr7Vj"
+    access_token_secret = "74KlJAkFvCdQJ5MclE8xUbzadtweTPfnmHE8H6MXdkoaP"
  
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
@@ -62,7 +60,11 @@ if __name__ == "__main__":
     # create Spark session
     spark = SparkSession.builder.appName("Twitter").getOrCreate()
     spark.conf.set("spark.sql.parquet.compression.codec", "gzip")
-
+    
+    #To avoid unncessary logs
+    #spark.setLogLevel("WARN")
+    
+   
     
     # Subscribe to test1 topic 
     lines = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "172.18.0.2:6667").option("subscribe", "test1").load()
@@ -72,7 +74,7 @@ if __name__ == "__main__":
     words = clean(lines)
     
     # text classification to define polarity and subjectivity
-    words = text_classification(words)
+    words = text_classification(words)    
     words = words.withColumn('sentiment',when( words.polarity >= 0, "positive").otherwise("negative"))
     words = words.repartition(1)   
     reply_query = words.writeStream.foreach(reply_to_tweet).start()
@@ -80,9 +82,9 @@ if __name__ == "__main__":
     #save files into hdfs    
     query = words.writeStream.queryName("all_tweets")\
         .outputMode("append").format("parquet")\
-        .option("path", "hdfs://sandbox-hdp.hortonworks.com:8020/user/root/twitter")\
-        .option("checkpointLocation","hdfs://sandbox-hdp.hortonworks.com:8020/user/root/check")\
+        .option("path", "hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/twitter.db/tweets")\
+        .option("checkpointLocation","hdfs://sandbox-hdp.hortonworks.com:8020/apps/hive/warehouse/twitter.db/check")\
         .trigger(processingTime='60 seconds').start()
+        
     query.awaitTermination()
     reply_query.awaitTermination()
-    
